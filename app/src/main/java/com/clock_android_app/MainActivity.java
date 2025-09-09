223a28fe828827bab0c1b841640af6f15e1dd0db
Thiago Candido <thiagocarvalhocandido8@gmail.com>
2025-09-09 11:04:16 -0300

    ok

--- app/src/main/java/com/clock_android_app/MainActivity.java
+++ app/src/main/java/com/clock_android_app/MainActivity.java
@@ -1,6 +1,9 @@
 package com.clock_android_app;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class MainActivity extends AppCompatActivity {
+
+
+
+    EditText timeNow;
+    EditText timeSeconds;
+
+    private Handler handler = new Handler(Looper.getMainLooper());
+    private Runnable clockRunnable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +34,38 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        timeNow = findViewById(R.id.timeNow);
+        timeSeconds = findViewById(R.id.timeSeconds);
+
+
+        clockRunnable = new Runnable() {
+            @Override
+            public void run() {
+                LocalDateTime time = LocalDateTime.now();
+
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+                String formattedTime = time.format(formatter);
+
+                DateTimeFormatter formatterSegundos = DateTimeFormatter.ofPattern("ss");
+                String formattedTime2 = time.format(formatterSegundos);
+
+                timeNow.setText(formattedTime);
+                timeSeconds.setText(formattedTime2);
+
+
+                handler.postDelayed(this, 1000);
+            }
+        };
+
+        handler.post(clockRunnable);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Stop updates when activity is destroyed
+        handler.removeCallbacks(clockRunnable);
     }
 }
