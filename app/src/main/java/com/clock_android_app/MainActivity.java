d277f98bd7889df84b93c86888f916d83f26a498
Thiago Candido <thiagocarvalhocandido8@gmail.com>
2025-09-13 11:12:54 -0300

    Setting view Binding

--- app/src/main/java/com/clock_android_app/MainActivity.java
+++ app/src/main/java/com/clock_android_app/MainActivity.java
@@ -1,5 +1,6 @@
 package com.clock_android_app;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -11,23 +12,31 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.clock_android_app.databinding.ActivityMainBinding;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private ActivityMainBinding _binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+
+        _binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        setContentView(_binding.getRoot());
+
+        ViewCompat.setOnApplyWindowInsetsListener(_binding.main, (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        _binding.
 
     }
 
